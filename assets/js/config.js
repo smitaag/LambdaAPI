d91aad72dede6a81fbe7fd5a041d854eaf446533
smitaag <smita.agarwal@outlook.com>
2022-07-03 21:40:38 +0530

    cognito check commented

---
+++ assets/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-south-1_oHYeHzmrN', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '19qfm8h7if1n5qj7ij0huveq5u', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://pfimbm1v6g.execute-api.ap-south-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
